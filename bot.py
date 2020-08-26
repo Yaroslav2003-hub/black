f6cb6bab3159af2fc519bf561e0fdebab8f9afc8
Yaroslav2003 <arikbujka@gmail.com>
2020-08-26 20:43:38 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telebot import types
 bot = telebot.TeleBot('1326123436:AAERCbZ2FNkCg_G0jqb59bAmsg_Rh2Tnd4c')
 
 conn = mysql.connector.connect(host='127.0.0.1', user='root', password='', port='3306', database='telegram')
-cur = connect.cursor()
+cur = conn.cursor()
 
 def insert_func(val):
     sql = "INSERT INTO users VALUES(%s, %s, %s, %s, %s)"
@@ -18,5 +18,5 @@ def start_func(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Добро пожаловать в казино!')
 
 
-if name == 'main':
+if __name__ == '__main__':
     bot.infinity_polling()
