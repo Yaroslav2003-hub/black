0a6b41ff0e07d6a639391591f86d4767e347a606
napkin3301 <BusinessNovichkov@yandex.ru>
2020-08-26 19:30:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,7 @@ bot = telebot.TeleBot('1326123436:AAERCbZ2FNkCg_G0jqb59bAmsg_Rh2Tnd4c')
 connect = mysql.connector.connect(host='127.0.0.1', user='root', password='', port='3306', database='telegram')
 cur = connect.cursor()
 
-def 
+def connection(val):
+    sql = "INSERT INTO users VALUES(%s, %s, %s, %s, %s)"
+    cur.execute(sql, val)
+    conn.commit()
