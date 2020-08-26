e27cb6f5877e2c09d87e792bf456185054e92f86
napkin3301 <BusinessNovichkov@yandex.ru>
2020-08-26 19:25:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,3 +3,7 @@ import mysql.connector
 
 bot = telebot.TeleBot('1326123436:AAERCbZ2FNkCg_G0jqb59bAmsg_Rh2Tnd4c')
 
+connect = mysql.connector.connect(host='127.0.0.1', user='root', password='', port='3306', database='users')
+cur = connect.cursor()
+
+def 
