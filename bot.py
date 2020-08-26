a1fc313ae180a806adb3078bd86c3043ff7df67a
Yaroslav2003 <arikbujka@gmail.com>
2020-08-26 20:56:12 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,16 +16,16 @@ def insert_func(val):
 
 @bot.message_handler(commands=['start'])
 def start_func(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Добро пожаловать в казино!')
     sql = ("SELECT * FROM users WHERE telegram_id = %s ")
     data = [message.chat.id]
     cur.execute(sql, data)
     result = cur.fetchall()
     if len(result) > 0:
-        pass
+        bot.send_message(message.chat.id, 'Здравствуйте! Добро пожаловать в казино!')
     else:
         data1 = [message.chat.id, 0, 0, 0, 0]
         insert_func(data1)
+        bot.send_message(message.chat.id, 'Здравствуйте! Добро пожаловать в казино!')
 
 
 
