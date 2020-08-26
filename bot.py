66f2900066a2a68b78b9404864818aa24f5c19e9
Yaroslav2003 <arikbujka@gmail.com>
2020-08-26 20:28:05 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 
 bot = telebot.TeleBot('1326123436:AAERCbZ2FNkCg_G0jqb59bAmsg_Rh2Tnd4c')
 
-connect = mysql.connector.connect(host='127.0.0.1', user='root', password='', port='3306', database='users')
+connect = mysql.connector.connect(host='127.0.0.1', user='root', password='', port='3306', database='telegram')
 cur = connect.cursor()
 
 def 
